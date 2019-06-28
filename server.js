5f8c5d6ac76f6481b263acb7e8f285ac3cd9ef31
TuanNT <nguyentientuan161090@gmail.com>
2019-06-28 21:37:54 +0700

    est66421231

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var moment = require('moment');
 var port = process.env.PORT || 3000;
 
 function handleRequest (request, response) {
-  response.end('[' + hostname + '] TuanNT at home imagePullPolicy 12345.Serving requests from myapp. Request URL:' + request.url);
+  response.end('[' + hostname + '] TuanNT at home imagePullPolicy xxxx.Serving requests from myapp. Request URL:' + request.url);
 }
 
 var server = http.createServer(handleRequest);
